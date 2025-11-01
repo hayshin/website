4c5e0e989da6ad633e0d1eb06f00070e4c8773a7
hayshin <hayshinbj@gmail.com>
2025-11-01 15:18:55 +0500

    refactor(locales): move structured content to home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,32 +7,23 @@
   const structuredData = {
     '@context': 'https://schema.org',
     '@type': 'Person',
-    'name': m.structured_data_person_name(),
-    'alternateName': m.structured_data_person_alternate_name(),
-    'jobTitle': m.structured_data_person_job_title(),
-    'description': m.structured_data_person_description(),
+    'name': m.home_structured_data_person_name(),
+    'alternateName': m.home_structured_data_person_alternate_name(),
+    'jobTitle': m.home_structured_data_person_job_title(),
+    'description': m.home_structured_data_person_description(),
     'url': 'https://hayshin.dev',
     'email': m.common_email(),
     'address': {
       '@type': 'PostalAddress',
-      'addressLocality': m.structured_data_person_address_locality(),
-      'addressCountry': m.structured_data_person_address_country(),
+      'addressLocality': m.home_structured_data_person_address_locality(),
+      'addressCountry': m.home_structured_data_person_address_country(),
     },
     'sameAs': [
       'https://github.com/hayshin',
       'https://linkedin.com/in/hayshin',
       'https://t.me/hayshinbj',
     ],
-    'knowsAbout': [
-      m.structured_data_skills_ai(),
-      m.structured_data_skills_ml(),
-      m.structured_data_skills_typescript(),
-      m.structured_data_skills_python(),
-      m.structured_data_skills_nixos(),
-      m.structured_data_skills_linux(),
-      m.structured_data_skills_software_engineering(),
-      m.structured_data_skills_web_development(),
-    ],
+    'knowsAbout': m.home_structured_data_knows_about().split(', '),
   };
 </script>
 
